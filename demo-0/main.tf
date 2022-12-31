ebc7eb950312c3cd4ddac8c5159c510b7471ba91
Clivern <hello@clivern.com>
2023-01-01 00:48:58 +0100

    update

---
+++ demo-0/main.tf
@@ -0,0 +1,29 @@
+
+// $ terraform console
+// var.name1
+// "${var.name1}"
+variable "name1" {
+  type = string
+  default = "Hello World"
+}
+
+variable "name2" {
+  type = map
+  description = "describe your map"
+  default = {
+    key1 = "val1"
+    key2 = "val2"
+  }
+}
+
+variable "name3" {
+  type = list
+  description = "describe your variable"
+  default = ["a", "b", 3]
+}
+
+variable "name4" {
+  type = bool
+  description = "describe your variable"
+  default = true
+}
